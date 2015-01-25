e6f434e9b016c7709e159c2afdae388792c7ea84
Max Novakovic <max@maxnov.com>
2015-01-25 12:32:21 +0000

    Add location to event

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -90,7 +90,8 @@ class LocationController < ApplicationController
   def format_event(event)
     time = event['time'].gsub(event['date'].to_s, '').gsub('00:00', '00').gsub('30:00', '30')
     date = event['date'].strftime("%^a %d %^b, #{time}")
-    { 'id' => event['str_id'], 'title' => event['event'],
+    return event
+    { 'id' => event['str_id'], 'title' => event['event'], 'location' => event['location'],
       'description' => event['description'], 'date' => date, 'distance' => event['distance'] }
   end
 
